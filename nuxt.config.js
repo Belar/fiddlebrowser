18db062c5c99a9668723cddb9d005274a0c971c3
Belar <belar@belardesign.com>
2016-12-23 12:32:06 +0100

    Add jsonp

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
     ]
   },
+  build: {
+    vendor: ['jsonp'] // Add jsonp in the vendor.bundle.js
+  },
   /*
   ** Global CSS
   */
