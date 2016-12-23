0edbdf52ce00302748b2a837a2b729d459da5500
Belar <belar@belardesign.com>
2016-12-23 14:03:40 +0100

    Format nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,40 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'fiddleBrowser',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', content: "jsFiddle public profile browser" }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
-    ]
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    }, {
+      hid: 'description',
+      content: "jsFiddle public profile browser"
+    }],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'favicon.ico'
+    }]
   },
   build: {
     vendor: ['jsonp'] // Add jsonp in the vendor.bundle.js
   },
   /*
-  ** Global CSS
-  */
-  css: [
-    { src: 'bulma', lang: 'sass' },
-    '~assets/css/main.css'],
+   ** Global CSS
+   */
+  css: [{
+      src: 'bulma',
+      lang: 'sass'
+    },
+    '~assets/css/main.css'
+  ],
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' }
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#3B8070'
+  }
 }
