41db9c871187671bceadc969c7ef56c1936d2dd8
Belar <belar@belardesign.com>
2016-12-31 19:46:19 +0100

    Move browser to a separate component, reuse

--- pages/index.vue
+++ pages/index.vue
@@ -5,80 +5,18 @@
       <h2 class="subtitle">jsFiddle profile browser</h2>
     </hero-title>
 
-    <div class="container">
-      <label for="username" class="label">Username</label>
-      <p class="control has-addons">
-        <input name="username" class="input is-expanded" type="text" v-model="nick" @keyup.enter="fetchFiddles()">
-        <button class="button is-primary" :class="{'is-loading': loading, 'is-disabled' : loading }" @click="fetchFiddles()">Fetch fiddles</button>
-      </p>
-      <div class="columns">
-        <div class="column is-8">
-          <label for="search" class="label">Search phrase</label>
-          <p class="control">
-            <input name="search" class="input" type="text" v-model="search">
-            <small>Searches through title, description and framework</small>
-          </p>
-        </div>
-        <div class="column">
-          <label for="limit" class="label">Fiddle limit</label>
-          <p class="control">
-            <span class="select is-fullwidth">
-              <select name="limit" v-model="limit" @change="fetchFiddles()">
-                <option :value="5">5</option>
-                <option :value="10">10</option>
-                <option :value="50">50</option>
-                <option :value="100">100</option>
-              </select>
-            </span>
-            <small>Auto-refetch included</small>
-          </p>
-        </div>
-      </div>
-      <projects-table :fiddles="fiddles" :search="search"></projects-table>
-    </div>
+    <browser></browser>
   </div>
 </template>
 
 <script>
-import jsonp from 'jsonp';
-
-import ProjectsTable from '~components/ProjectsTable';
 import HeroTitle from '~components/HeroTitle';
+import Browser from '~components/Browser';
 
 export default {
   components: {
-    ProjectsTable,
-    HeroTitle
-  },
-  data: function() {
-    return {
-      nick: '',
-      search: '',
-      limit: 10,
-      fiddles: [],
-      loading: false
-    }
-  },
-  methods: {
-    fetchFiddles: function() {
-      if (this.fiddles.length > this.limit) {
-        return this.fiddles.splice(this.limit);
-      }
-      if (this.nick && this.limit > 0) {
-        this.loading = true;
-
-        jsonp('https://jsfiddle.net/api/user/' + this.nick + '/demo/list.json?limit=' + this.limit, {
-          timeout: 5000
-        }, (err, data) => {
-          if (err) {
-            this.loading = false;
-          } else {
-            this.fiddles = data.list;
-            this.loading = false;
-          }
-        });
-      }
-    }
+    HeroTitle,
+    Browser
   }
 }
 </script>
